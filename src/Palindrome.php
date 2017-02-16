99612a8541ee5ca26bafd0252c0aaab6ade1555c
Jason Brown and Koji Nakagawa <koji@>
2017-02-15 16:20:01 -0800

    create test to pass third spec for two words

--- src/Palindrome.php
+++ src/Palindrome.php
@@ -23,8 +23,15 @@
         function checkPalindrome()
         {
             $input1 = preg_replace('/' . " " . '/i', "", $this->input);
-            $input2 = strrev($input1);
-            var_dump($input2);
+
+            $input1_array = str_split($input1, 1);
+            $input2_array = array();
+            var_dump(count($input1_array));
+            for($i=count($input1_array)-1; $i>=0; $i--){
+            array_push($input2_array,  $input1_array[$i]);
+            }
+
+            $input2 = implode($input2_array);
             if ($input2 == $input1) {
                 return true;
             } else {
